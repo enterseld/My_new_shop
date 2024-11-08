6d220eece05025636b33875638f0eb8bb8f7cf4f
Enterseld <enterseld@gmail.com>
2024-11-08 12:20:18 +0200

    Cart page and view in cart controller

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import UserLayout from './Layouts/UserLayout.vue';
 import { Link, router } from '@inertiajs/vue3';
+import Hero from './Layouts/Hero.vue';
+
 defineProps({
     products: Array
 })
@@ -26,6 +28,9 @@ const addToCart = (product) => {
 
 <template>
     <UserLayout>
+        <!--Hero section-->
+        <Hero></Hero>
+        <!--End-->
         <div class="bg-white">
             <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 class="text-2xl font-bold tracking-tight text-gray-900">Latest products</h2>
